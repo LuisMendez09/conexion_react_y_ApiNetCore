414f2c4ee21eb8a46fe3ec3a87960458179e7e3d
Luis Mendez <luis009alberto@gmail.com>
2021-08-27 12:57:51 -0700

    conexion entre react y asp.net core

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios, { AxiosResponse } from 'axios';
+import React, { useEffect } from 'react';
 import './App.css';
+import { urlClimas } from './endpoints';
 
 function App() {
+  useEffect(()=>{
+    axios.get(urlClimas)
+      .then((respuesta: AxiosResponse<any>) => {
+        console.log(respuesta.data);
+      });
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>aplicacion react</h1>
+      <p>comunicacion entre ASP.et core y react</p>
+    </>
   );
 }
 
